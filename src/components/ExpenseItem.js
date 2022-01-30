052873e444eb3e9cbe6112a63c345c8d5fd5a8d0
tambarie <gt.gbaragbo@gmail.com>
2022-01-31 00:17:01 +0100

    add to file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+  return (
+    <>
+      <div className='expense-item'>
+        March 28th 2021
+        <div className='expense-item__description'>
+          <h2>Car Insurance</h2>
+          <div className='expense-item__price'>$295.98</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ExpenseItem
